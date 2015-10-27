cfd9873219fe00fa97de368232d39f82c517337a
David Clark <plotinussmith@gmail.com>
2015-10-27 01:44:29 -0500

    Added more registry functionality.

--- src/main/java/db/postgresql/async/pginfo/PgType.java
+++ src/main/java/db/postgresql/async/pginfo/PgType.java
@@ -1,29 +1,38 @@
 package db.postgresql.async.pginfo;
 
+import java.util.Collections;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.SortedSet;
-
+import static db.postgresql.async.types.UdtHashing.*;
+    
 public class PgType {
 
     public static final String DEFAULT_SCHEMA = "public";
     public static final int DEFAULT_RELID = 0;
     public static final char DEFAULT_DELIMITER = ',';
-    
+
+    final private short registry;
     final private int oid;
     final private NameKey nameKey;
     final private int arrayId;
     final private int relId;
     final private char delimiter;
+    final private SortedSet<PgAttribute> attributes;
 
     private PgType(final int oid, final int arrayId, final NameKey nameKey,
-                   final int relId, final char delimiter) {
+                   final int relId, final char delimiter, final SortedSet<PgAttribute> attributes, final short registry) {
         this.oid = oid;;
         this.arrayId = arrayId;
         this.nameKey = nameKey;
         this.relId = relId;
         this.delimiter = delimiter;
+        this.attributes = attributes;
+        this.registry = registry;
     }
 
     public static Builder builder() { return new Builder(); }
+    public static Builder builder(final PgType pgType) { return new Builder(pgType); }
 
     public static class Builder {
         private String schema = DEFAULT_SCHEMA;
@@ -32,6 +41,8 @@ public class PgType {
         private int arrayId;
         private int relId = DEFAULT_RELID;
         private char delimiter = DEFAULT_DELIMITER;
+        private short registry;
+        private SortedSet<PgAttribute> attributes = new TreeSet<>();
 
         public Builder schema(final String val) { schema = val; return this; }
         public Builder name(final String val) { name = val; return this; }
@@ -39,9 +50,13 @@ public class PgType {
         public Builder arrayId(final int val) { arrayId = val; return this; }
         public Builder relId(final int val) { relId = val; return this; }
         public Builder delimiter(final char val) { delimiter = val; return this; }
-
+        public Builder registry(final short val) { registry = val; return this; }
+        public Builder attribute(final PgAttribute val) { attributes.add(val); return this; }
+        
         public PgType build() {
-            return new PgType(oid, arrayId, NameKey.immutable(schema, name), relId, delimiter);
+            return new PgType(oid, arrayId, NameKey.immutable(schema, name), relId, delimiter,
+                              attributes.size() == 0 ? Collections.emptySortedSet() : attributes,
+                              registry);
         }
 
         public Builder() { }
@@ -53,6 +68,8 @@ public class PgType {
             this.arrayId = pgType.getArrayId();
             this.relId = pgType.getRelId();
             this.delimiter = pgType.getDelimiter();
+            this.attributes.addAll(pgType.getAttributes());
+            this.registry = pgType.registry;
         }
     }
 
@@ -74,12 +91,12 @@ public class PgType {
         }
 
         PgType o = (PgType) rhs;
-        return oid == o.oid;
+        return oid == o.oid && registry == o.registry;
     }
 
     @Override
     public int hashCode() {
-        return oid;
+        return hash(hash(START, oid), (int) registry);
     }
 
     @Override
@@ -94,16 +111,12 @@ public class PgType {
             .append("delimiter: '" + getDelimiter() + "', ")
             .append("complex: " + isComplex() + ", ")
             .append("builtin: " + isBuiltin() + ", ")
+            .append("registry: " + registry + ", ")
             .append("attributes: " + getAttributes() + ")").toString();
     }
 
     public SortedSet<PgAttribute> getAttributes() {
-        if(isComplex()) {
-            throw new UnsupportedOperationException();
-        }
-        else {
-            return null;
-        }
+        return attributes;
     }
 }
 
---
+++ src/main/java/db/postgresql/async/pginfo/PgTypeRegistry.java
@@ -0,0 +1,45 @@
+package db.postgresql.async.pginfo;
+
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import static db.postgresql.async.pginfo.PgType.*;
+import static db.postgresql.async.pginfo.NameKey.threadLocal;
+
+public class PgTypeRegistry {
+
+    private static final AtomicInteger counter = new AtomicInteger(1);
+
+    private final short id;
+    public short getId() { return id; }
+    
+    private final ConcurrentMap<Integer,PgType> oidMap = new ConcurrentHashMap<>();
+    private final ConcurrentMap<NameKey,PgType> nameMap = new ConcurrentHashMap<>();
+
+    public PgTypeRegistry() {
+        this.id = (short) counter.getAndIncrement();
+    }
+
+    public PgTypeRegistry add(final PgType val) {
+        final PgType pgType = builder(val).registry(id).build();
+        oidMap.put(pgType.getOid(), pgType);
+        nameMap.put(pgType.getNameKey(), pgType);
+        return this;
+    }
+
+    public PgType byOid(final Integer oid) {
+        return oidMap.get(oid);
+    }
+
+    public PgType byName(final String schema, final String name) {
+        return byName(threadLocal(schema, name));
+    }
+
+    public PgType byName(final String fullName) {
+        return byName(threadLocal(fullName));
+    }
+
+    public PgType byName(final NameKey key) {
+        return nameMap.get(key);
+    }
+}
