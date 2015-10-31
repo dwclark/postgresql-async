995739e36a2c5754da4556ce97b7fac4a52f21d2
David Clark <plotinussmith@gmail.com>
2015-10-31 00:17:44 -0500

    Added local date serializer

---
+++ src/main/java/db/postgresql/async/serializers/LocalDateSerializer.java
@@ -0,0 +1,36 @@
+package db.postgresql.async.serializers;
+
+import db.postgresql.async.pginfo.PgType;
+import java.nio.ByteBuffer;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import static db.postgresql.async.serializers.SerializationContext.*;
+
+public class LocalDateSerializer extends Serializer<LocalDate> {
+
+    private static final String STR = "uuuu-MM-dd";
+    private static final DateTimeFormatter DATE = DateTimeFormatter.ofPattern(STR);
+
+    public static final PgType PGTYPE =
+        new PgType.Builder().name("date").oid(1082).arrayId(1182).build();
+
+    public Class<LocalDate> getType() { return LocalDate.class; }
+
+    public static final LocalDateSerializer instance = new LocalDateSerializer();
+    
+    public LocalDate fromString(final String str) {
+        return LocalDate.parse(str, DATE);
+    }
+    
+    public String toString(final LocalDate val) {
+        return (val == null) ? null : val.format(DATE);
+    }
+    
+    public LocalDate read(final ByteBuffer buffer, final int size) {
+        return isNull(size) ? null : fromString(bufferToString(buffer, size));
+    }
+
+    public void write(final ByteBuffer buffer, final LocalDate val) {
+        stringToBuffer(buffer, val.format(DATE));
+    }
+}
