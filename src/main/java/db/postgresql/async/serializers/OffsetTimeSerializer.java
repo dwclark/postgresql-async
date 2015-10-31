d3643db74404814653c268bfe443c35d0952bba1
David Clark <plotinussmith@gmail.com>
2015-10-31 00:39:06 -0500

    Added offset time serializer.

---
+++ src/main/java/db/postgresql/async/serializers/OffsetTimeSerializer.java
@@ -0,0 +1,37 @@
+package db.postgresql.async.serializers;
+
+import db.postgresql.async.pginfo.PgType;
+import java.nio.ByteBuffer;
+import java.time.OffsetTime;
+import java.time.format.DateTimeFormatter;
+import static db.postgresql.async.serializers.SerializationContext.*;
+
+public class OffsetTimeSerializer extends Serializer<OffsetTime> {
+    
+    private static final String STR = "HH:mm:ss.nx";
+    private static final DateTimeFormatter DATE = DateTimeFormatter.ofPattern(STR);
+
+    public static final PgType PGTYPE =
+         new PgType.Builder().name("timetz").oid(1266).arrayId(1270).build();
+
+    public static final OffsetTimeSerializer instance = new OffsetTimeSerializer();
+    
+    public Class<OffsetTime> getType() { return OffsetTime.class; }
+
+    public OffsetTime fromString(final String str) {
+        final int index = str.lastIndexOf('-');
+        return OffsetTime.parse(str.substring(0, index) + "000" + str.substring(index), DATE);
+    }
+
+    public String toString(final OffsetTime val) {
+        return val == null ? null : val.format(DATE);
+    }
+
+    public OffsetTime read(final ByteBuffer buffer, final int size) {
+        return isNull(size) ? null : fromString(bufferToString(buffer, size));
+    }
+
+    public void write(final ByteBuffer buffer, final OffsetTime val) {
+        stringToBuffer(buffer, val.format(DATE));
+    }
+}
