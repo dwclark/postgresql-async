9e66b6058f50e50daa2c0c9de10c65e0fb5720bf
David Clark <plotinussmith@gmail.com>
2015-10-31 00:24:11 -0500

    Added local date time serializer.

---
+++ src/main/java/db/postgresql/async/serializers/LocalDateTimeSerializer.java
@@ -0,0 +1,36 @@
+package db.postgresql.async.serializers;
+
+import db.postgresql.async.pginfo.PgType;
+import java.nio.ByteBuffer;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import static db.postgresql.async.serializers.SerializationContext.*;
+
+public class LocalDateTimeSerializer extends Serializer<LocalDateTime> {
+
+    private static final String STR = "uuuu-MM-dd HH:mm:ss.n";
+    private static final DateTimeFormatter DATE = DateTimeFormatter.ofPattern(STR);
+
+    public static final LocalDateTimeSerializer instance = new LocalDateTimeSerializer();
+
+    public static final PgType PGTYPE =
+        new PgType.Builder().name("timestamp").oid(1114).arrayId(1115).build();
+
+    public Class<LocalDateTime> getType() { return LocalDateTime.class; }
+    
+    public LocalDateTime fromString(final String str) {
+        return LocalDateTime.parse(str + "000", DATE);
+    }
+
+    public String toString(final LocalDateTime val) {
+        return val == null ? null : val.format(DATE);
+    }
+    
+    public LocalDateTime read(final ByteBuffer buffer, final int size) {
+        return isNull(size) ? null : fromString(bufferToString(buffer, size));
+    }
+
+    public void write(final ByteBuffer buffer, final LocalDateTime date) {
+        stringToBuffer(buffer, date.format(DATE));
+    }
+}
