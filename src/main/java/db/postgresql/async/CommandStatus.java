3a285cfd21fdf24439767789c03eb4d66bc08a20
David Clark <plotinussmith@gmail.com>
2015-11-09 23:48:50 -0600

    Added method to interface

--- src/main/java/db/postgresql/async/CommandStatus.java
+++ src/main/java/db/postgresql/async/CommandStatus.java
@@ -3,6 +3,7 @@ package db.postgresql.async;
 public interface CommandStatus {
     String getAction();
     boolean isMutation();
+    boolean isSelect();
     boolean hasRows();
     int getRows();
     boolean hasOid();
